8e0971257100dc1ae7e486d6e75ad1344518dc31
techai1236-boop <techai1236@gmail.com>
2026-09-03 16:09:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,5 @@ export default defineConfig(() => {
         '@': path.resolve(__dirname, '.'),
       },
     },
-    server:{ 
-      proxy: { '/api': { target: 'http://localhost:3000', changeOrigin: true, secure: false } }
-    },
   };
 });
